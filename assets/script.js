1c384b9f89621a12156e5af252f2878e021cf0fd
Jaweriasaleem54 <125985442+Jaweriasaleem54@users.noreply.github.com>
2023-10-25 13:40:22 +0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function getData() {
+    var fullName = document.getElementById("fullName").value;
+    var Phone= document.getElementById("Phone").value;
+    var Address = document.getElementById("Address").value;
+    var Email = document.getElementById("Email").value;
+
+    console.log("First Name:", fullName);
+    console.log("Email:", Phone);
+    console.log("Contact Number:", Address);
+    console.log("Comments:", Email);
+}
